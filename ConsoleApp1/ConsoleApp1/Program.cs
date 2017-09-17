150482cea51827d78a6e7508a53a2d187ebe0152
Sanjay Rawat <irawatsanjay@gmail.com>
2017-09-17 05:39:34 -0400

    Update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            System.Console.WriteLine("Welcome to Devops");
+            System.Console.WriteLine("Welcome to Devops 1");
             System.Console.ReadLine();
         }
     }
